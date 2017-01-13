893250077f6c0cb2196877e622dd44ae9316c322
3rror404 <daniel_rochelle@Dans-iMac.local>
2017-01-13 17:02:59 +0000

    ActionBar background now works. Back nav button does not.

--- app/main-page.js
+++ app/main-page.js
@@ -3,6 +3,21 @@ var frameModule = require("ui/frame");
 function onNavigatingTo(args) {
 
     var page = args.object;
+
+    if (page.ios) {
+		var controller = frameModule.topmost().ios.controller;
+
+		/**
+		 * Return actionbar to default after being transaprent
+		 */
+		// var navBar = controller.navigationBar;
+		// navBar.setBackgroundImageForBarMetrics(null, UIBarMetricsDefault);
+        // navBar.shadowImage = null;
+
+        // //navBar.translucent = false;
+		// navBar.barTintColor = null;
+
+	}
 }
 exports.onNavigatingTo = onNavigatingTo;
 
--- app/second-page.js
+++ app/second-page.js
@@ -4,6 +4,7 @@ exports.pageLoaded = function(args) {
     var page = args.object;
 
 	if (page.ios) {
+
 		var controller = frameModule.topmost().ios.controller;
 
 		/**
@@ -11,16 +12,37 @@ exports.pageLoaded = function(args) {
 		 */
 		var navBar = controller.navigationBar;
 		navBar.shadowImage = new UIImage();
-		navBar.setBackgroundImageForBarMetrics(new UIImage(), UIBarMetrics.UIBarMetricsDefault);
+		navBar.setBackgroundImageForBarMetrics(new UIImage, UIBarMetrics.UIBarMetricsDefault);
 
 		/**
-		 * Fade in ActionBar on scroll
+		 * Add custom view to navBar
+		 */
+		var navBounds = navBar.bounds;
+		var myView = UIView.alloc().init();
+		myView.frame = {
+			origin: { x: navBounds.origin.x, y: navBounds.origin.y - 20 },
+			size: { width: navBounds.size.width, height: navBounds.size.height + 20 }
+		};
+		myView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+		navBar.addSubview(myView);
+
+		navBar.backgroundColor = UIColor.colorWithRedGreenBlueAlpha(0.20, 0.20, 0.20, 0.0);
+		navBar.sendSubviewToBack(myView);
+
+		/**
+		 * Fade in myView on scroll
 		 */
 		var scrollView = page.getViewById("scrollView");
 		scrollView.on('scroll', function(args){
 			var offset = args.object.verticalOffset;
-			navBar.backgroundColor = UIColor.colorWithRedGreenBlueAlpha(0.20, 0.20, 0.20, (offset-50)/50);
+			myView.backgroundColor = UIColor.colorWithRedGreenBlueAlpha(0.20, 0.20, 0.20, (offset-50)/50);
 		});
+
 	}
 	
+}
+
+
+exports.tapGoBack = function() {
+	frameModule.topmost().goBack();
 }
